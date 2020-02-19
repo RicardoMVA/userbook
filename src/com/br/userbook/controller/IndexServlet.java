49a69fdf425477cbd22c76ebedc2294e37b98711
RicardoMVA <ricardo-valenca@hotmail.com>
2020-02-19 16:33:29 -0300

    make simple index servlet and jsp

---
+++ src/com/br/userbook/controller/IndexServlet.java
@@ -0,0 +1,25 @@
+package com.br.userbook.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public IndexServlet() {
+		super();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher reqDis = request.getRequestDispatcher("index.jsp");
+		reqDis.forward(request, response);
+	}
+
+}
