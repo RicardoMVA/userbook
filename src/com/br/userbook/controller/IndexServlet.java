2d78eb8b1f50c97ccb3d666d2e1e2ab5c17e61fe
RicardoMVA <ricardo-valenca@hotmail.com>
2020-02-22 11:57:17 -0300

    create new servlet for 'user'

--- src/com/br/userbook/controller/IndexServlet.java
+++ src/com/br/userbook/controller/IndexServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.br.userbook.model.Phone;
 import com.br.userbook.model.User;
 import com.br.userbook.dao.UserDao;
 
@@ -24,17 +23,9 @@ public class IndexServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String action = request.getServletPath();
-
 		try {
-			switch (action) {
-			case "/new":
-				showCreateUser(request, response);
-				break;
-			default:
-				showAllUsers(request, response);
-				break;
-			}
+			showAllUsers(request, response);
+
 		} catch (ServletException ex) {
 			request.setAttribute("error", ex.getMessage());
 			RequestDispatcher reqDis = request.getRequestDispatcher("/error.jsp");
@@ -42,50 +33,11 @@ public class IndexServlet extends HttpServlet {
 		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String name = request.getParameter("name");
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
-
-		User newUser = new User(name, email, password);
-
-		userDao.createUser(newUser);
-
-		String[] ddds = request.getParameterValues("ddd");
-		String[] phones = request.getParameterValues("phone");
-		String[] types = request.getParameterValues("type");
-
-		for (int i = 0; i < phones.length; i++) {
-			int ddd = 0;
-
-			try {
-				ddd = Integer.parseInt(ddds[i]);
-			} catch (NumberFormatException e) {
-				throw new ServletException("Invalid DDD number.");
-			}
-
-			String phone = phones[i];
-			String type = types[i];
-
-			Phone newPhone = new Phone(newUser, ddd, phone, type);
-			userDao.createPhone(newPhone);
-		}
-
-		response.sendRedirect("/");
-	}
-
 	protected void showAllUsers(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		List<User> listUser = userDao.getUsers();
 		request.setAttribute("listUser", listUser);
 		RequestDispatcher reqDis = request.getRequestDispatcher("landing.jsp");
-		reqDis.forward(request, response);
-	}
-
-	private void showCreateUser(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		RequestDispatcher reqDis = request.getRequestDispatcher("user/new.jsp");
 		reqDis.forward(request, response);
 	}
 
---
+++ src/com/br/userbook/controller/UserServlet.java
@@ -0,0 +1,82 @@
+package com.br.userbook.controller;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.br.userbook.model.Phone;
+import com.br.userbook.model.User;
+import com.br.userbook.dao.UserDao;
+
+@WebServlet("/users/*")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private UserDao userDao;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getPathInfo();
+		
+		try {
+			switch (action) {
+			case "/new":
+				showCreateUser(request, response);
+				break;
+			default:
+				response.sendRedirect("/");
+				break;
+			}
+		} catch (ServletException ex) {
+			request.setAttribute("error", ex.getMessage());
+			RequestDispatcher reqDis = request.getRequestDispatcher("/error.jsp");
+			reqDis.forward(request, response);
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		User newUser = new User(name, email, password);
+
+		userDao.createUser(newUser);
+
+		String[] ddds = request.getParameterValues("ddd");
+		String[] phones = request.getParameterValues("phone");
+		String[] types = request.getParameterValues("type");
+
+		for (int i = 0; i < phones.length; i++) {
+			int ddd = 0;
+
+			try {
+				ddd = Integer.parseInt(ddds[i]);
+			} catch (NumberFormatException e) {
+				throw new ServletException("Invalid DDD number.");
+			}
+
+			String phone = phones[i];
+			String type = types[i];
+
+			Phone newPhone = new Phone(newUser, ddd, phone, type);
+			userDao.createPhone(newPhone);
+		}
+
+		response.sendRedirect("/");
+	}
+	
+	private void showCreateUser(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher reqDis = request.getRequestDispatcher("/user/new.jsp");
+		reqDis.forward(request, response);
+	}
+}
