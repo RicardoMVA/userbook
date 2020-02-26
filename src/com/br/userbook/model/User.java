342fe4078ca82934f3a05e2447a5c17cbb961947
RicardoMVA <ricardo-valenca@hotmail.com>
2020-02-26 16:25:53 -0300

    add constraints to 'user' class

--- src/com/br/userbook/model/User.java
+++ src/com/br/userbook/model/User.java
@@ -11,6 +11,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import com.br.userbook.model.Phone;
 
@@ -20,13 +25,25 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
+	@NotNull(message = "Name cannot be null")
+	@NotEmpty(message = "Name cannot be empty")
+//	blocks all numbers and non-latin characters
+	@Pattern(regexp = "[^0-9]*[^\\P{L}]*", message = "Name contains invalid characters")
+	@Size(min = 4, max = 80, message = "Name cannot have less than 4 or more than 80 characters")
 	@Column(nullable = false, length = 80)
 	private String name;
-
+	
+	@NotNull(message = "Email cannot be null")
+	@NotEmpty(message = "Email cannot be empty")
+	@Email(message = "Must be a valid email address")
+	@Size(min = 4, max = 80, message = "Email cannot have less than 4 or more than 80 characters")
 	@Column(nullable = false, unique = true, length = 80)
 	private String email;
-
+	
+	@NotNull(message = "Password cannot be null")
+	@NotEmpty(message = "Password cannot be empty")
+	@Size(min = 8, max = 80, message = "Password cannot have less than 8 or more than 80 characters")
 	@Column(nullable = false, length = 80)
 	private String password;
 
