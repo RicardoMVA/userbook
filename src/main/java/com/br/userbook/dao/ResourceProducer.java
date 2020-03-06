e83b85e15ee6909271a8834e49076565ec8ae2c2
RicardoMVA <ricardo-valenca@hotmail.com>
2020-03-06 16:30:09 -0300

    fix resource producer missing 'unitName' in persistencecontext annotation

--- src/main/java/com/br/userbook/dao/ResourceProducer.java
+++ src/main/java/com/br/userbook/dao/ResourceProducer.java
@@ -1,11 +1,13 @@
 package com.br.userbook.dao;
 
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+@RequestScoped
 public class ResourceProducer {
-	@PersistenceContext
+	@PersistenceContext(unitName = "userbookdb")
 	@Produces
 	private EntityManager em;
 
